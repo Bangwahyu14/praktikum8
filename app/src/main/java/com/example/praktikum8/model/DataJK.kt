179188e6ff93ed320ce788e02172fbc1c9e89414
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-20 12:50:04 +0700

    menambahkan folder model dengan isi datajk dan siswa

---
+++ app/src/main/java/com/example/praktikum8/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum8.model
+
+import com.example.praktikum8.R
+
+object DataJK {
+    val JenisJK= listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
---
+++ app/src/main/java/com/example/praktikum8/model/Siswa.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum8.model
+
+data class Siswa {
+    val nama : string ="",
+    val gender: string ="",
+    val alamat: =""
+
+
+}
