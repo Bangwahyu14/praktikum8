58e8c1a25e74f6d1e47680cf184ecba759603d30
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-20 12:25:12 +0700

    membuat file beranda dan membuat halaman beranda

---
+++ app/src/main/java/com/example/praktikum8/ui/theme/view/Beranda.kt
@@ -0,0 +1,22 @@
+package com.example.praktikum8.ui.theme.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Beranda(
+    onSubmitClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
